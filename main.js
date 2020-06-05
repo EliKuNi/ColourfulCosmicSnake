f2db0002734b6edb51e794809baf82694e829b30
Elizabeth <65304506+ElizabethKuleshova@users.noreply.github.com>
2020-06-05 20:47:28 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+alert("✨Cute adorable colourful cosmic snake✨\n\n✨Bet you won't be able to resist its charm✨")
+
+setInterval(blink,1000);
+
+function blink() {
+    $("#blink").toggleClass("changeColor");
+}
